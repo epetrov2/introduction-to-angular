e49722c45fd6e92e395e0a6fe5c2ac27e65a9690
Viktor Lachkov <EPetrov2@gmail.com>
2022-10-14 16:05:06 +0400

    Add treasure locations

---
+++ src/app/treasure-location.ts
@@ -0,0 +1,9 @@
+export interface TreasureLocation {
+    name: string,
+    city: string,
+    state: string,
+    photo: string,
+    availableCoins: number,
+    cursed: boolean,
+    diamonds: boolean
+}
---
+++ src/app/treasures-mock.ts
@@ -0,0 +1,31 @@
+import { TreasureLocation } from "./treasure-location";
+
+export const treasureLocationList: TreasureLocation[] = [
+{
+    name: "Cave with treasure chest",
+    city: "Chicago",
+    state: "IL",
+    photo: "../assets/4008 fhd.jpg",
+    availableCoins: 1000,
+    cursed: true,
+    diamonds: false
+},
+{
+    name: "Deep sea underwater",
+    city: "Santa Monica",
+    state: "CA",
+    photo: "../assets/9Z_2101.w026.n002.112B.p681.112_fhd.jpg",
+    availableCoins: 999,
+    cursed: true,
+    diamonds: true
+},
+{
+    name: "Pirate Island",
+    city: "Juneau",
+    state: "AK",
+    photo: "../assets/29Z_2105.w026.n002.415B.p1.415 fhd.jpg",
+    availableCoins: 1000,
+    cursed: false,
+    diamonds: false
+}
+]
