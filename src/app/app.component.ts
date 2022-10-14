cb4097d7aa2582e5fe07af7930116161aaedd306
Viktor Lachkov <EPetrov2@gmail.com>
2022-10-14 16:12:51 +0400

    Add treasure location to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { TreasureLocation } from './treasure-location';
+import { treasureLocationList } from './treasures-mock';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Treasure';
+
+  treasureLocationList: TreasureLocation[] = treasureLocationList;
+
 }
--- src/app/treasures-list/treasures-list.component.ts
+++ src/app/treasures-list/treasures-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
+import { TreasureLocation } from "../treasure-location";
 
 @Component({
   selector: 'app-treasures-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreasuresListComponent implements OnInit {
 
+  @Input() locationList: TreasureLocation[] = [];
   constructor() { }
 
   ngOnInit(): void {
